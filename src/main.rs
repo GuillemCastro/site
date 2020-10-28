e760dcd217d134aa5fb84073aa426e6386d18569
Guillem Castro <guillemcastro4@gmail.com>
2020-10-29 00:29:01 +0100

    Add main page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 
 include!(concat!(env!("OUT_DIR"), "/templates.rs"));
-use crate::templates::{blogpost_html, Html};
+use crate::templates::{blogpost_html, Html, index_html};
 
 use color_eyre::eyre::{Result};
+
+use std::fs;
 
 pub mod post;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let mut buf = Vec::new();
+    index_html(&mut buf)?;
+    let index = String::from_utf8(buf)?;
+    println!("{}", index);
+    
+    fs::write("index.html", index)?;
+
     let posts = post::load("blog")?;
     for post in posts {
         let mut buf = Vec::new();
